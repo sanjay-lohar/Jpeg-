4f21ba6ad53e158d7261fb726c6fcc11b42d1139
sanjay-lohar <37002242+sanjay-lohar@users.noreply.github.com>
2018-06-09 22:58:03 +0530

    Create ha.php

---
+++ ha.php
@@ -0,0 +1,43 @@
+{
+	"heroes": [{
+			"id": "1",
+			"name": "You’re really not hot enough to get away with being this boring"
+		},
+		{
+			"id": "2",
+			"name": "Sanjay"
+		},
+		{
+			"id": "3",
+			"name": "Lohar"
+		},
+		{
+			"id": "4",
+			"name": "happy"
+		},
+		{
+			"id": "5",
+			"name": "boring"
+		},
+		{
+			"id": "6",
+			"name": "laugh"
+		},
+		{
+			"id": "7",
+			"name": "sad"
+		},
+		{
+			"id": "8",
+			"name": "cry"
+		},
+		{
+			"id": "9",
+			"name": "angry"
+		},
+		{
+			"id": "10",
+			"name": "bye"
+		}
+	]
+}
